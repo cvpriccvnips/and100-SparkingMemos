08cf3a5b1951ec6eb7cb7317a67647775c02febc
Cecil <yuuuno32@gmail.com>
2022-03-04 20:41:41 +0800

    init

---
+++ app/src/main/java/com/example/cecilsflashcardapp/MainActivity.java
@@ -0,0 +1,32 @@
+
+package com.example.cecilsflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+//import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView questionText=findViewById(R.id.flashcard_question_textview);
+        TextView flashcardAnswer=findViewById(R.id.flashcard_answer_textview);
+
+        questionText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+//              Log.i("pulina","entered onCLick method");
+//              Toast.makeText(MainActivity.this,"I click the question",Toast.LENGTH_LONG).show();
+                questionText.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+}
